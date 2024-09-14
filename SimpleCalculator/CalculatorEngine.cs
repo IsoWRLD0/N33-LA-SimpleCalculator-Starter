ee60df4fa86abe57af8a9d2f5381cc3daa5e32e6
Kevin Reonegro <115087159+IsoWRLD0@users.noreply.github.com>
2024-09-13 21:42:38 -0400

    Update CalculatorEngine.cs

--- SimpleCalculator/CalculatorEngine.cs
+++ SimpleCalculator/CalculatorEngine.cs
@@ -1,15 +1,41 @@
-﻿using System;
+using System;
 
 namespace SimpleCalculator
 {
     public class CalculatorEngine
     {
-        public double Calculate (string argOperation, double argFirstNumber, double argSecondNumber)
+        public double Calculate(string argOperation, double argFirstNumber, double argSecondNumber)
         {
             double result = 0;
 
-            if (argOperation == "+" || argOperation.ToLower() == "add") {
-                result = argFirstNumber + argSecondNumber;
+            switch (argOperation.ToLower())
+            {
+                case "+":
+                case "add":
+                    result = argFirstNumber + argSecondNumber;
+                    break;
+                case "-":
+                case "subtract":
+                    result = argFirstNumber - argSecondNumber;
+                    break;
+                case "*":
+                case "multiply":
+                    result = argFirstNumber * argSecondNumber;
+                    break;
+                case "/":
+                case "divide":
+                    if (argSecondNumber == 0)
+                    {
+                        throw new DivideByZeroException("Cannot divide by zero");
+                    }
+                    result = argFirstNumber / argSecondNumber;
+                    break;
+                case "power":
+                case "pow":
+                    result = Math.Pow(argFirstNumber, argSecondNumber);
+                    break;
+                default:
+                    throw new ArgumentException("Invalid operation");
             }
 
             return result;
